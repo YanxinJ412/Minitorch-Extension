735293146100788f8557936ace56dcbb93545566
Shizhuo Li <sli31@br013.ib.bridges2.psc.edu>
2026-03-30 15:21:40 -0400

    eric_test_init

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -4,6 +4,8 @@ import minitorch.scalar_functions as scalar_functions  # noqa: F401,F403
 
 from .autodiff import *  # noqa: F401,F403
 from .cuda_ops import *  # noqa: F401,F403
+
+from .kv_cache import *
 # from .cuda_kernel_ops import *
 from .datasets import datasets  # noqa: F401,F403
 from .fast_conv import *  # noqa: F401,F403
